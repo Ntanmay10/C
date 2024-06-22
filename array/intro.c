7ddd8d718e2ca1a79c5b1fba2680024b1a945ee7
Ntanmay10 <findtanmay10@gmail.com>
2024-06-22 14:26:21 +0530

    22-06-2024

--- array/intro.c
+++ array/intro.c
@@ -24,6 +24,11 @@ void main()
     printf("The value of a[2] is %d\n",a[2]);
     printf("The value of a[3] is %d\n",a[3]);
     printf("The value of a[4] is %d\n",a[4]);
+
+    OR
+
+    int a[5] = {1,2,3,4,5};
+    char name[6] = {'T','A','N','M','A','Y'};
     */
 
     // Taking input from used and displaying the same
@@ -137,4 +142,123 @@ void main()
             printf("No even numbers entered.\n");
         }
     */
+
+    // Take input in array and display the lowest of all and its location
+    /*
+    int i, n, low, lc = 0, a[5];
+    system("cls");
+
+    // Input values into array
+    printf("Enter 5 integers:\n");
+    for (i = 0; i < 5; i++)
+    {
+        printf("Enter the value of a[%d]: ", i);
+        scanf("%d", &a[i]);
     }
+
+    // Initialize low with the first element
+    low = a[0];
+
+    // Find the lowest value and its location
+    for (i = 1; i < 5; i++)
+    {
+        if (a[i] < low)
+        {
+            low = a[i];
+            lc = i;
+        }
+    }
+
+    // Output the lowest value and its location
+    printf("The lowest of all inputted numbers is %d and its location is a[%d]\n", low, lc);
+    */
+
+    // Take an array input and also print the reverse of the array
+    /*
+    int i, a[3], b[3];
+    system("cls");
+
+    for (i = 0; i < 3; i++)
+    {
+        printf("Enter the value of a[%d] :", i);
+        scanf("%d", &a[i]);
+        b[2 - i] = a[i];
+    }
+
+    for (i = 0; i < 3; i++)
+        printf("The value of a[%d] & b[%d] is \n", a[i], b[i]);
+    */
+
+    // Take 2 array as input and display thier sum
+    /*
+    int i, a[3], b[3], sum[3];
+    system("Cls");
+
+    for (i = 0; i < 3; i++)
+    {
+        printf("Enter the value of a[%d]: ", i);
+        scanf("%d", &a[i]);
+    }
+    for (i = 0; i < 3; i++)
+    {
+        printf("Enter the value of b[%d]: ", i);
+        scanf("%d", &b[i]);
+    }
+
+    for (i = 0; i < 3; i++)
+    {
+        sum[i] = a[i] + b[i];
+    }
+
+    for (i = 0; i < 3; i++)
+        printf("The sum of a[%d] + b[%d] is sum[%d] \n", a[i], b[i], sum[i]);
+    */
+
+    // Take array as input and display the difference between the greates and lowest number
+    /*
+    int i, low = 0, grt = 0, a[5];
+    system("cls");
+
+    for (i = 0; i < 5; i++)
+    {
+        printf("Enter the value of a[%d]: ", i);
+        scanf("%d", &a[i]);
+    }
+    low = a[0];
+
+    for (i = 0; i < 5; i++)
+    {
+        if (a[i] < low)
+        {
+            low = a[i];
+        }
+        if (a[i] > grt)
+        {
+            grt = a[i];
+        }
+    }
+    printf("The greatest is %d and the lowest is %d and the difference is %d ", grt, low, grt - low);
+    */
+
+    // Reversing the original array
+    /*
+    int i, b, a[5];
+    system("cls");
+
+    for (i = 0; i < 5; i++)
+    {
+        printf("Enter the value of a[%d]: ", i);
+        scanf("%d", &a[i]);
+    }
+
+    for (i = 0; i < 5 / 2; i++)
+    {
+        b = a[i];
+        a[i] = a[4 - i];
+        a[4 - i] = b;
+    }
+
+    for (i = 0; i < 5; i++)
+        printf("The reversed array is %d\n", a[i]);
+    */
+}
