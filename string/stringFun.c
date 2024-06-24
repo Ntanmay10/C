72fae03e884734610d8fc318aba3a4a86e4a912f
Ntanmay10 <findtanmay10@gmail.com>
2024-06-24 14:22:27 +0530

    24-06-2024

--- string/stringFun.c
+++ string/stringFun.c
@@ -8,3 +8,44 @@
 // 6.strrev () => Reverses the string
 // 7.strlwr () => converts the string to lower case
 // 8.strupr () => converts the string to upper case
+
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void main()
+{   
+    //Example 1: To check if the entered string is pelindrome or not
+    /*
+    char str[30], rev[30];
+    int l;
+    system("cls");
+
+    printf("Enter the string to be checked: ");
+    gets(str);
+
+    strcpy(rev, str);
+    strrev(rev);
+    l = strcmp(str, rev);
+
+    if (l == 0)
+    {
+        printf("The entered string is pelindrome");
+    }
+    else
+    {
+        printf("The entered string isn't prlindrome");
+    }
+    */
+
+    //Example 2: To convert entire string into upper case
+    char str[30];
+    system("cls");
+
+    printf("Enter your string: ");
+    gets(str);
+
+    strupr(str);
+    puts(str);
+}
