fb4bf66b82f75e30e99543bc508f56ff01231772
Ntanmay10 <findtanmay10@gmail.com>
2024-06-25 09:07:36 +0530

    25-06-2024

--- master_program.c
+++ master_program.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-#include <conio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
+// Define the structure and typedef
 struct data
 {
     int roll;
@@ -16,92 +17,157 @@ struct data
 
 typedef struct data dta;
 
-void header(char[]);
-void line(int, char);
-void title();
-void centre(char[]);
-void main()
+// Initialize the array of structures with data
+dta a[3] = {
+    {1, "Tanmay", "Amar", "Naik", "Samrawadi", "BCA", 'M'},
+    {2, "Chirag", "Dilip", "Patel", "Vijalpor", "BBA", 'M'},
+    {3, "Mahek", "Niraj", "Naik", "Gandevi", "BCom", 'F'}};
+
+// Function prototypes for decorative parts
+void displayMenu();
+void printHeader();
+void printFooter();
+void printTableHeader();
+
+int main()
 {
-    dta a[3] = {{01, "Tanmay", "Amar", "Naik", "Samrawadi", "BCA", "M"},
-                {02, "Chirag", "Dilip", "Patel", "Vijalpor", "BBA", "M"},
-                {03, "Mahek", "Niraj", "Naik", "Gandevi", "BCom", "F"}};
-    char gen, cty[15], crs[10], name[50];
-    int i, rno, op;
-    system("cls");
+    int choice, rno;
+    char gend, cty[15], crs[10];
 
-    header("<<Students Data>>");
-    title();
-
-    for (i = 0; i < 3; i++)
+    do
     {
-        printf("\n%3d %-10s %-10s %-10s %-5s %-2c", a[i].roll, a[i].fname, a[i].mname, a[i].lname, a[i].city, a[i].course, a[i].gen);
-    }
+        printHeader();
+        displayMenu();
 
-    printf("\n\n Enter 1 For course wise detail \n Enter 2 for city wise detail");
-    scanf("%d", &op);
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
 
-    for (i = 0; i < 2; i++)
-    {
-        if (op == 1)
+        if (choice == 1)
         {
+            // Display all records
             system("cls");
-            header("<<Course Wise>>");
-            printf("Enter the course: ");
-            gets(crs);
-            title();
-            for (i = 0; i < 3; i++)
+            printTableHeader();
+            for (int i = 0; i < 3; i++)
+            {
+                printf("%-5d %-10s %-10s %-10s %-15s %-10s %-5c\n", a[i].roll, a[i].fname, a[i].mname, a[i].lname, a[i].city, a[i].course, a[i].gen);
+            }
+        }
+        else if (choice == 2)
+        {
+            // Search by roll number
+             system("cls");
+            printf("Enter roll number: ");
+            scanf("%d", &rno);
+            printTableHeader();
+            for (int i = 0; i < 3; i++)
+            {
+                if (a[i].roll == rno)
+                {
+                    printf("%-5d %-10s %-10s %-10s %-15s %-10s %-5c\n", a[i].roll, a[i].fname, a[i].mname, a[i].lname, a[i].city, a[i].course, a[i].gen);
+                    break;
+                }
+            }
+        }
+        else if (choice == 3)
+        {
+            // Search by gender
+             system("cls");
+            printf("Enter gender (M/F): ");
+            scanf(" %c", &gend);  // Note the space before %c to consume any leftover newline character
+            gend = toupper(gend); // Convert to uppercase for case-insensitive comparison
+            printTableHeader();
+            for (int i = 0; i < 3; i++)
+            {
+                if (toupper(a[i].gen) == gend)
+                {
+                    printf("%-5d %-10s %-10s %-10s %-15s %-10s %-5c\n", a[i].roll, a[i].fname, a[i].mname, a[i].lname, a[i].city, a[i].course, a[i].gen);
+                }
+            }
+        }
+        else if (choice == 4)
+        {
+            // Search by city
+             system("cls");
+            printf("Enter city: ");
+            scanf("%s", cty);
+            for (int i = 0; cty[i]; i++)
+            {
+                cty[i] = tolower(cty[i]);
+            }
+            printTableHeader();
+            for (int i = 0; i < 3; i++)
+            {
+                char cityLower[15];
+                strcpy(cityLower, a[i].city);
+                for (int j = 0; cityLower[j]; j++)
+                {
+                    cityLower[j] = tolower(cityLower[j]);
+                }
+                if (strcmp(cityLower, cty) == 0)
+                {
+                    printf("%-5d %-10s %-10s %-10s %-15s %-10s %-5c\n", a[i].roll, a[i].fname, a[i].mname, a[i].lname, a[i].city, a[i].course, a[i].gen);
+                }
+            }
+        }
+        else if (choice == 5)
+        {
+            // Search by course
+             system("cls");
+            printf("Enter course: ");
+            scanf("%s", crs);
+            printTableHeader();
+            for (int i = 0; i < 3; i++)
             {
                 if (strcmpi(a[i].course, crs) == 0)
                 {
-                    printf("\n%3d %-10s %-10s %-10s %-5s %-2c", a[i].roll, a[i].fname, a[i].mname, a[i].lname, a[i].city, a[i].course, a[i].gen);
+                    printf("%-5d %-10s %-10s %-10s %-15s %-10s %-5c\n", a[i].roll, a[i].fname, a[i].mname, a[i].lname, a[i].city, a[i].course, a[i].gen);
                 }
             }
         }
-        else if (op == 2)
+        else if (choice == 0)
         {
-            system("cls");
-            header("<<City Wise>>");
-            printf("Enter the city: ");
-            gets(cty);
-            title();
-            for (i = 0; i < 3; i++)
-            {
-                if (strcmpi(a[i].city, cty) == 0)
-                {
-                    printf("\n%8d %-10s %-10s %-10s %-6s %-3c", a[i].roll, a[i].fname, a[i].mname, a[i].lname, a[i].city, a[i].course, a[i].gen);
-                }
-            }
+            printf("Exiting the program.\n");
+            printFooter();
+            exit(0);
         }
-    }
+        else
+        {
+            printf("Invalid choice! Please try again.\n");
+        }
+
+        printFooter();
+    } while (choice != 0);
+
+    return 0;
 }
 
-void line(int l, char ch)
+void displayMenu()
 {
-    int i;
-    printf("\n");
-    for (i = 0; i < l; i++)
-    {
-        printf("%c", ch);
-    }
+    printf("\nMenu:\n");
+    printf("1. Display all records\n");
+    printf("2. Search by roll number\n");
+    printf("3. Search by gender\n");
+    printf("4. Search by city\n");
+    printf("5. Search by course\n");
+    printf("0. Exit\n");
 }
 
-void centre(char str[])
+void printHeader()
 {
-    int i;
-    for (i = 0; i < 40 - strlen(str) / 2; i++)
-    {
-        printf(" ");
-    }
-    printf("%s\n", str);
+    printf("\n====================\n");
+    printf("Welcome to the Menu\n");
+    printf("====================\n");
 }
 
-void header(char str[])
+void printFooter()
 {
-    line(80, 241);
-    centre(str);
-    line(80, 241);
+    printf("====================\n");
+    printf("Thank you!\n");
+    printf("====================\n");
 }
 
-void title(){
-    printf("%8s %-10s %-10s %-10s %-6s %-3s","Roll No","F Name","M Name","L Name","City","Course","Sex");
+void printTableHeader()
+{
+    printf("\n%-5s %-10s %-10s %-10s %-15s %-10s %-5s\n", "Roll", "First Name", "Middle Name", "Last Name", "City", "Course", "Gender");
+    printf("-------------------------------------------------------------------------------------\n");
 }
